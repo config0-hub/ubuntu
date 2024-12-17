18c7dc98746565d1dc46f5adb68556ce726c2847
gary <gary>
2024-12-17 11:32:27 +0800

    rendered and updated 12-17-2024-11-32

---
+++ execgroups/_config0_configs/18.04-docker/_actions/10-install_docker_pkgs.py
@@ -0,0 +1,9 @@
+def default():
+
+    return {
+        'method': 'shelloutconfig',
+        'metadata': {
+            'env_vars': [],
+            'shelloutconfigs': ['config0-publish:::ubuntu::install-docker-pkgs']
+            }
+    }
